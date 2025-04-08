c18121670c27d2751aec062d08504e1971eba7ee
Aime <aimeca011.com>
2025-04-07 22:49:41 -0600

    Mostrar alumnos

--- app.py
+++ app.py
@@ -35,45 +35,5 @@ def listar_alumnos():
     alumnos = Alumno.query.all()
     return render_template('index.html', alumnos=alumnos)
 
-# Crear un nuevo estudiante (formulario)
-@app.route('/alumnos/new', methods=['GET', 'POST'])
-def create_alumno():
-    if request.method == 'POST':
-        no_control = request.form['no_control']
-        nombre = request.form['nombre']
-        ap_paterno = request.form['ap_paterno']
-        ap_materno = request.form['ap_materno']
-        semestre = request.form['semestre']
-
-        nuevo_alumno = Alumno(no_control=no_control, nombre=nombre, ap_paterno=ap_paterno, ap_materno=ap_materno, semestre=semestre)
-        db.session.add(nuevo_alumno)
-        db.session.commit()
-
-        return redirect(url_for('listar_alumnos'))
-    return render_template('create_alumno.html')
-
-# Actualizar un alumno (formulario)
-@app.route('/alumnos/update/<string:no_control>', methods=['GET', 'POST'])
-def update_alumno(no_control):
-    alumno = Alumno.query.get(no_control)
-    if request.method == 'POST':
-        alumno.nombre = request.form['nombre']
-        alumno.ap_paterno = request.form['ap_paterno']
-        alumno.ap_materno = request.form['ap_materno']
-        alumno.semestre = request.form['semestre']
-        
-        db.session.commit()
-        return redirect(url_for('listar_alumnos'))
-    return render_template('update_alumno.html', alumno=alumno)
-
-# Eliminar un alumno
-@app.route('/alumnos/delete/<string:no_control>')
-def delete_alumno(no_control):
-    alumno = Alumno.query.get(no_control)
-    if Alumno:
-        db.session.delete(alumno)
-        db.session.commit()
-    return redirect(url_for('listar_alumnos'))
-
 if __name__ == '__main__':
     app.run(debug=True)
